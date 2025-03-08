85578610f07304de38b61212d277927ff0344b28
Let0oro <oniiichan354@gmail.com>
2025-03-08 13:48:52 +0100

    reparing player reference failing

--- server/config/db.js
+++ server/config/db.js
@@ -16,10 +16,6 @@ const checkDBConnection = async () => {
       "SELECT TRUE AS connected_to_database;"
     );
     console.log(rows);
-    const { rowsP } = await pool.query(
-      "\dt;"
-    );
-    console.log(rowsP);
     const { rowCount: exist } = await pool.query("SELECT * FROM player;")
     if (!exist) await createTablesAndSeed();
   } catch (error) {
@@ -29,20 +25,24 @@ const checkDBConnection = async () => {
 
 
 const createTablesAndSeed = async () => {
-  await pool.query("DROP TABLE IF EXISTS player CASCADE");
-  await pool.query("DROP TABLE IF EXISTS player_session");
-  await pool.query("DROP TABLE IF EXISTS store CASCADE ");
-  await pool.query("DROP TABLE IF EXISTS score");
-  await pool.query("DROP TABLE IF EXISTS likes");
-  await pool.query("DROP TABLE IF EXISTS ship");
-  await pool.query(queryTables.player);
-  await pool.query(queryTables.store);
-  await pool.query(queryTables.likes);
-  await pool.query(queryTables.ship);
-  await pool.query(queryTables.score);
+  try {
+    await pool.query("DROP TABLE IF EXISTS player CASCADE");
+    await pool.query("DROP TABLE IF EXISTS player_session");
+    await pool.query("DROP TABLE IF EXISTS store CASCADE ");
+    await pool.query("DROP TABLE IF EXISTS score");
+    await pool.query("DROP TABLE IF EXISTS likes");
+    await pool.query("DROP TABLE IF EXISTS ship");
+    await pool.query(queryTables.player);
+    await pool.query(queryTables.store);
+    await pool.query(queryTables.likes);
+    await pool.query(queryTables.ship);
+    await pool.query(queryTables.score);
 
-  await generateCSV();
-  await insertData();
+    await generateCSV();
+    await insertData();
+  } catch (error) {
+    console.error("Error creating tables or seeding data:", error);
+  }
 };
 checkDBConnection();
 
